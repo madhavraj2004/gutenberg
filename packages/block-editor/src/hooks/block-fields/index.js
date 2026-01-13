521d1e897e2cbf983ef213221c2d60363cb7bef6
Daniel Richards <daniel.richards@automattic.com>
2026-01-13 15:51:45 +0800

    Remove forceDisplayControls in favor of mayDisplayPatternEditingControls context value

--- packages/block-editor/src/hooks/block-fields/index.js
+++ packages/block-editor/src/hooks/block-fields/index.js
@@ -242,10 +242,7 @@ const withBlockFields = createHigherOrderComponent(
 					isSelectionWithinCurrentSection &&
 						( isSectionBlock ||
 							blockEditingMode === 'contentOnly' ) && (
-							<PrivateInspectorControlsFill
-								group="content"
-								forceDisplayControls
-							>
+							<PrivateInspectorControlsFill group="content">
 								<BlockFields
 									{ ...props }
 									blockType={ blockType }
